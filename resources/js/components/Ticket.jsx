a42cffc688ff23fb17a1208fa1924e6e7513985c
Chatmalow <maxime.pie.mail@gmail.com>
2020-05-11 21:26:04 +0200

    feat(front): Add searchable select to help selecting the ingredients

--- resources/js/components/Ticket.jsx
+++ resources/js/components/Ticket.jsx
@@ -2,23 +2,32 @@ import React from 'react';
 import {useParams} from "react-router-dom";
 import CarbsLine from "./CarbsLine";
 import Loading from "./Loading";
+import Collapse from '@material-ui/core/Collapse';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
 import { useHistory } from "react-router-dom";
 
 export default function Ticket(props) {
     const history = useHistory();
+
+    const [productsList, setProductsList] = React.useState([]);
 
     const [ticket, setTicket] = React.useState(undefined);
     const [productName, setProductName] = React.useState('');
     const [portion, setPortion] = React.useState(1);
     const [carbsPerHundred, setCarbsPerHundred] = React.useState(undefined);
     const [gramsPerPortion, setGramsPerPortion] = React.useState(undefined);
+
     const [isLoading, setIsLoading] = React.useState(false);
+    const [areDetailsExplained, setDetailsExplanation] = React.useState(false);
 
     const [isExpanded, setExpansion] = React.useState(false);
     let {id} = useParams();
 
     React.useEffect(() => {
-        fetchData();
+        fetchTicketData();
+        fetchProducts();
     }, [id]);
 
     return (
@@ -31,32 +40,63 @@ export default function Ticket(props) {
                             <h3 className="Ticket__title">{ticket.user.name}</h3>
                             <i className="Ticket__header-action fas fa-user" onClick={goToUserProfile}/>
                             <i className="Ticket__header-action fas fa-sync" onClick={goToCurrentTicket}/>
+                            <i className="Ticket__header-action Ticket__header-action--trigger fas fa-question-circle"
+                               onClick={() => setDetailsExplanation(!areDetailsExplained)}
+                            />
                         </div>
-                        <div className="Ticket__details">
-                            <p className="Ticket__detail">
-                                <span className="Ticket__subdetail">
-                                    Maximum: {ticket.target}
-                                </span>
-                                <span className="Ticket__subdetail">
-                                    Actuel: {ticket.current}
-                                </span>
-                            </p>
-                            <p className="Ticket__detail Ticket__detail--big">
-                                Restant: {ticket.target - ticket.current}
-                            </p>
-                        </div>
+                        <Collapse in={areDetailsExplained}>
+                            <div className="Ticket__actions-details">
+                                <div className="Ticket__actions-detail">
+                                    <i className="Ticket__actions-detail-icon fas fa-user"/>
+                                    Consulter le profil de {ticket.user.name}
+                                </div>
+                                <div className="Ticket__actions-detail">
+                                    <i className="Ticket__actions-detail-icon fas fa-sync"/>
+                                    Afficher le ticket du jour
+                                </div>
+                            </div>
+                        </Collapse>
+                        <Collapse in={!areDetailsExplained}>
+                            <div className="Ticket__details">
+                                <p className="Ticket__detail">
+                                    <span className="Ticket__subdetail">
+                                        Maximum: {ticket.target}
+                                    </span>
+                                    <span className="Ticket__subdetail">
+                                        Actuel: {ticket.current}
+                                    </span>
+                                </p>
+                                <p className="Ticket__detail Ticket__detail--big">
+                                    Restant: {ticket.target - ticket.current}
+                                </p>
+                            </div>
+                        </Collapse>
                     </div>
                     <div className="Ticket__body">
                         {ticket.lines.map(line =>
                             <CarbsLine line={line} onDeleteLine={deleteLine} onIncrementLine={incrementLine}/>
                             )}
                         <div className="Ticket__fields">
-                            <input
-                                className="Ticket__fields-input"
-                                value={productName}
-                                placeholder="Aliment"
-                                type="text"
-                                onChange={(event) => setProductName(event.target.value)}
+                            {/*<input*/}
+                            {/*    className="Ticket__fields-input"*/}
+                            {/*    value={productName}*/}
+                            {/*    placeholder="Aliment"*/}
+                            {/*    type="text"*/}
+                            {/*    onChange={(event) => setProductName(event.target.value)}*/}
+                            {/*/>*/}
+                            <Autocomplete
+                                id="free-solo-demo"
+                                freeSolo
+                                options={productsList}
+                                renderInput={(params) => (
+                                    <TextField
+                                        {...params}
+                                        label="Aliment"
+                                        margin="normal"
+                                        variant="outlined"
+                                        onChange={(event) => setProductName(event.target.value)}
+                                    />
+                                )}
                             />
                             <div className="Ticket__fields-input Ticket__fields-input--with-sub-parts">
                                 <input
@@ -113,11 +153,25 @@ export default function Ticket(props) {
     /**
      * Fetch the data of the ticket
      */
-    function fetchData() {
+    function fetchTicketData() {
         setIsLoading(true);
         axios.get('/api/tickets/' + id)
             .then(response => {
                 setTicket(response.data);
+                setIsLoading(false);
+            }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    /**
+     * Fetch the list of products names
+     */
+    function fetchProducts() {
+        setIsLoading(true);
+        axios.get('/api/products')
+            .then(response => {
+                setProductsList(response.data.map((product) => product.name));
                 setIsLoading(false);
             }).catch(err => {
             console.log(err)
@@ -156,7 +210,7 @@ export default function Ticket(props) {
         setIsLoading(true);
         axios.get('/api/carbsLine/delete/' + lineId)
             .then((response) => {
-                fetchData();
+                fetchTicketData();
             }).catch(err => {
             console.log(err)
         });
@@ -171,7 +225,7 @@ export default function Ticket(props) {
         setIsLoading(true);
         axios.get('/api/carbsLine/increment/' + lineId)
             .then((response) => {
-                fetchData();
+                fetchTicketData();
             }).catch(err => {
             console.log(err)
         });
