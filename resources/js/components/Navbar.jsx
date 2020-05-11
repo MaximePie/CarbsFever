62644c5d84bb7d806d6d525511084757b051a681
Chatmalow <maxime.pie.mail@gmail.com>
2020-05-11 18:55:35 +0200

    feat(front): Add back button

--- resources/js/components/Navbar.jsx
+++ resources/js/components/Navbar.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-export default function Navbar(props) {
+export default function Navbar() {
+    const history = useHistory()
+
     return (
         <div className="Navbar">
-            <Link to='/' className="Navbar__link">Tickets</Link>
-            <Link to='/products' className="Navbar__link">Ingredients</Link>
-            <Link to='/profile/default_profile' className="Navbar__link">Profil</Link>
+            <i className="Navbar__back fas fa-arrow-left" onClick={redirectToPreviousPage}/>
+            <div className="Navbar__links">
+                <Link to='/' className="Navbar__link">Tickets</Link>
+                <Link to='/products' className="Navbar__link">Ingredients</Link>
+                <Link to='/profile/default_profile' className="Navbar__link">Profil</Link>
+            </div>
         </div>
     );
+
+    /**
+     * Redirects the user to the previous url based on history
+     */
+    function redirectToPreviousPage() {
+        history.goBack();
+    }
 }
